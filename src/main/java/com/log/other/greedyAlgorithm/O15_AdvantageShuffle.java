d625093313f647a0602d1a98485a1a9ac5262844
Log <2664203975@qq.com>
2022-08-14 15:47:10 +0800

    2022-08-14-commit05

--- src/main/java/com/log/other/greedyAlgorithm/O15_AdvantageShuffle.java
+++ src/main/java/com/log/other/greedyAlgorithm/O15_AdvantageShuffle.java
@@ -5,7 +5,7 @@ import java.util.*;
 /**
  * @Author Log
  * @Date 2022/8/14 14:30
- * TODO 优势洗牌(田忌赛马)
+ * TODO 优势洗牌(田忌赛马) --贪心算法
  * 给定两个大小相等的数组nums1和nums2，nums1相对于 nums的优势可以用满足
  * nums1[i] > nums2[i]的索引 i的数目来描述。
  * 返回 nums1的任意排列，使其相对于 nums2的优势最大化。
